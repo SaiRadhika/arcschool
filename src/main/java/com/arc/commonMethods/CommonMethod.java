5be5c737cc8529b6ccab5bec784bce509c54a60a
Narayan Datt <narayandatt@promantus.com>
2021-10-10 19:02:59 +0530

    testing screenshot issue

--- src/main/java/com/arc/commonMethods/CommonMethod.java
+++ src/main/java/com/arc/commonMethods/CommonMethod.java
@@ -48,7 +48,7 @@ public class CommonMethod extends BaseClass {
 		log.info("takeScreenshotTest method starts here ......");
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
-		ScreenshotPath = currentDir + "//Screenshots//" + MethodName;
+		ScreenshotPath = currentDir + "/Screenshots/" + MethodName;
 		try {
 			FileUtils.copyFile(scrFile, new File(ScreenshotPath + ".png"));
 			log.info("Screenshot captured successfully for Method ...." + MethodName);
